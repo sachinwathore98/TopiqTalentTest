3ed509a4403599f3c041cc34d371abca56d71139
Sachin Wathore <sachinwathore98@gmail.com>
2026-08-04 15:27:07 +0530

    Add Brevo

--- backend/src/models/StudentModel.js
+++ backend/src/models/StudentModel.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: false },
   phone: { type: String, required: true },
   studentClass: { type: String, required: true },
-  pincode: { type: String },
-  city: { type: String },
+  pincode: { type: String, required: false },
+  city: { type: String, required: false },
   district: { type: String, required: true },
-  state: { type: String },
+  state: { type: String, required: false },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
 });
